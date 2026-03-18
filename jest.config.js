a65ced65cb14e09b4763b61e5311de5f6b2c1282
Ziad <ziad.morsy1998@hotmail.com>
2026-03-18 17:12:08 +0100

    added jest config

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+};
